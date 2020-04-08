5fa51e5dc5d5cd5062eda029b18e978e0f88d4cd
Valdemar Pereira <valdemar.developer@gmail.com>
2020-04-08 16:57:05 +0100

    Adding logs and integration with Zipkin

--- src/main/java/com/valdemar/codenameduet/playerservice/adapters/controller/PlayerServiceController.java
+++ src/main/java/com/valdemar/codenameduet/playerservice/adapters/controller/PlayerServiceController.java
@@ -4,6 +4,7 @@ import com.valdemar.codenameduet.playerservice.adapters.controller.request.Regis
 import com.valdemar.codenameduet.playerservice.adapters.controller.response.PlayerDetailResponse;
 import com.valdemar.codenameduet.playerservice.model.Player;
 import com.valdemar.codenameduet.playerservice.ports.PlayerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/player")
+@Slf4j
 public class PlayerServiceController {
 
     private PlayerService playerService;
@@ -28,16 +30,23 @@ public class PlayerServiceController {
 
     @PostMapping(value = "")
     ResponseEntity<?> registerPlayer(UriComponentsBuilder uriComponentsBuilder, @RequestBody RegisterPlayerRequest request) {
+
+        log.info("Request for register a new player: {}", request.getPlayerName());
+
         long id = playerService.registerPlayer(request.getPlayerName());
 
         UriComponents uriComponents =
                 uriComponentsBuilder.path("/player/{id}").buildAndExpand(id);
+
+        log.info("Player {} registered", request.getPlayerName());
 
         return ResponseEntity.created(uriComponents.toUri()).build();
     }
 
     @GetMapping("/{playerId}")
     PlayerDetailResponse playerDetails(@PathVariable long playerId) {
+        log.info("Request for getting details of the existing player Id: {}", playerId);
+
         Player player = playerService.playerDetails(playerId);
 
         return PlayerDetailResponse.builder().playerName(player.getName()).build();
--- src/main/java/com/valdemar/codenameduet/playerservice/service/PlayerServiceImpl.java
+++ src/main/java/com/valdemar/codenameduet/playerservice/service/PlayerServiceImpl.java
@@ -3,7 +3,9 @@ package com.valdemar.codenameduet.playerservice.service;
 import com.valdemar.codenameduet.playerservice.model.Player;
 import com.valdemar.codenameduet.playerservice.ports.PlayerRepository;
 import com.valdemar.codenameduet.playerservice.ports.PlayerService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public final class PlayerServiceImpl implements PlayerService {
 
     private final PlayerRepository playerRepository;
@@ -14,11 +16,14 @@ public final class PlayerServiceImpl implements PlayerService {
 
     @Override
     public long registerPlayer(final String playerName) {
+        log.info("Registering a new player: {}", playerName);
         return playerRepository.createPlayer(playerName);
     }
 
     @Override
     public Player playerDetails(long playerId) {
+        log.info("Getting player details for id: {}", playerId);
+
         return playerRepository.playerDetails(playerId);
     }
 }
